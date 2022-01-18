100bfdd274a7025fc2d1f29af7d100f94ee40d87
Loïc <monzaemon77.ll@gmail.com>
2022-01-18 14:07:04 +0100

    Install mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,33 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://Monza:tAOZSJ3TBh5YQgDU@cluster0.a2dzo.mongodb.net/P6Database?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use((req, res, next) => {
+  console.log("Requête reçue !");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: "Votre requête a bien été reçue !" });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Réponse envoyée avec succès !");
+});
 
 module.exports = app;
